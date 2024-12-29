a1a34dc95d2d530abc7af57692a9004cba9e299f
joabesnts09 <joabesilva1233@gmail.com>
2024-12-28 22:59:33 -0300

    feat: add multiple creation in Articles & Events

--- articles/serializers.py
+++ articles/serializers.py
@@ -2,8 +2,18 @@ from rest_framework import serializers
 from .models import Articles
 
 
+class BulkArticlesSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        articles = [Articles(**item) for item in validated_data]
+        return Articles.objects.bulk_create(articles)
+
 class ArticlesSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Articles
         fields = '__all__'
+        list_serializer_class = BulkArticlesSerializer
+
+
+    def create(self, validated_data):
+        return Articles.objects.create(**validated_data)
--- events/serializers.py
+++ events/serializers.py
@@ -2,8 +2,18 @@ from rest_framework import serializers
 from .models import Events
 
 
+class BulkEventsSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        Events = [Events(**item) for item in validated_data]
+        return Events.objects.bulk_create(Events)
+
+
 class EventsSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Events
         fields = '__all__'
+        list_serializer_class = BulkEventsSerializer
+        
+    def create(self, validated_data):
+        return Events.objects.create(**validated_data)
