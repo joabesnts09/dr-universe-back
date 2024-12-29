9ea723b5c98714fe5b9108a20481c9484c392311
joabesnts09 <joabesilva1233@gmail.com>
2024-12-28 23:56:14 -0300

    feat: add reflesh token in rotes & configure times

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from accounts.views import AccountView
 from rest_framework_simplejwt import views
 
+
 urlpatterns = [
-    path('accounts/', AccountView.as_view()),
-    path('login/', views.TokenObtainPairView.as_view()),
+    path('accounts/', AccountView.as_view(), name='create_account'),
+    path('login/', views.TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
